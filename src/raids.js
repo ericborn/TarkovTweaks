9c1418af4e92f4bc33bff1c8223659ba3d1fc615
Eric Born <eric.born85@gmail.com>
2021-12-28 22:30:58 -0600

    implement raid timer and boss spawn chance

--- src/raids.js
+++ src/raids.js
@@ -6,16 +6,44 @@ class mod
 {
     static onLoad()
     {
+        // initalize constants
         const DB = DatabaseServer.tables
         const locations = DB.locations
-        if (config.Raids.EnableExtendedRaid)
+
+        // enable extended raid if true and raidTime is a number
+        if (config.Raids.EnableExtendedRaid && typeof config.Raids.raidTimer === 'number')
         {
+            Logger.info("Hitman Mod: Extended Raid activated");
+            // for all maps if the map is not a base
             for (let map in locations) 
             {
                 if (map !== "base") 
                 {
-                    locations[map].base.exit_access_time = 240
-                    locations[map].base.escape_time_limit = 240
+                    // set exit and escape time to raidTimer
+                    locations[map].base.exit_access_time = config.Raids.raidTimer
+                    locations[map].base.escape_time_limit = config.Raids.raidTimer
+                }
+            }
+        }
+        // enable increased boss spawn if true and spawn percent is a number
+        if (config.Raids.IncreasedBossChance && typeof config.Raids.BossSpawnPercent === 'number')
+        {
+            // for all locations
+            for (let i in locations) 
+            {
+                // if not equal to base
+                if (i !== "base") 
+                {
+                    // if boss spawn location is not empty
+                    if (locations[i].base.BossLocationSpawn !== []) 
+                    {
+                        // set spawn chance
+                        for (let x in locations[i].base.BossLocationSpawn) 
+                        {
+                            locations[i].base.BossLocationSpawn[x].BossChance = config.raids.BossSpawnPercent
+                        }
+                        
+                    }
                 }
             }
         }
