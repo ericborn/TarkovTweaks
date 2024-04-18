218531ecf25939d2ce406f5824575795468d4084
Eric Born <eric.born85@gmail.com>
2024-04-18 18:52:02 -0500

    expanded features

--- mod.js
+++ mod.js
@@ -4,6 +4,7 @@ const flea = require("./src/flea.js");
 const insurance = require("./src/insurance.js");
 const raid = require("./src/raids.js");
 const trade = require("./src/traders.js");
+const items = require("./src/items.js");
 
 class Mod {
     constructor() {
@@ -12,6 +13,7 @@ class Mod {
         ModLoader.onLoad["TarkovTweaks Insurance"] = insurance.onLoad;
         ModLoader.onLoad["TarkovTweaks Trade"] = trade.onLoad;
         ModLoader.onLoad["TarkovTweaks Raid"] = raid.onLoad;
+        ModLoader.onLoad["TarkovTweaks Items"] = items.onLoad;
     }
 }
 
